430d6c50bdacb5b02ca839e4d7b170ed923eb73e
Manish10022001 <manishshirsat2850@gmail.com>
2025-12-30 19:47:00 +0530

    Routing: 2.triggering 404 function and specific 404 pages for section

--- routing/src/app/products/[productId]/reviews/[reviewsId]/page.tsx
+++ routing/src/app/products/[productId]/reviews/[reviewsId]/page.tsx
@@ -1,6 +1,14 @@
+import { notFound
+
+ } from "next/navigation";
 export default async function ReviewsDetails(
     {params} : {params:Promise<{productId:string, reviewsId:string}>}
 ){
     const {productId, reviewsId} = (await params);
+
+    if(parseInt(reviewsId) > 1000){
+        notFound();
+    }
+
     return <h3>{reviewsId} Review about Product {productId}</h3>
 }
